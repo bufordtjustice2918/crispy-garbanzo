c74d613febf01ed067657287e5b5e009a9a4f6c2
kavansmith <kavansmith@gmail.com>
2026-02-18 05:24:47 -0800

    Use GRUB bootloaders only for Ubuntu 24.04 ISO build

--- build/iso/scripts/build-livecd.sh
+++ build/iso/scripts/build-livecd.sh
@@ -27,6 +27,7 @@ lb config \
   --distribution "${DISTRO}" \
   --architectures "${ARCH}" \
   --binary-images iso-hybrid \
+  --bootloaders "grub-efi grub-pc" \
   --bootappend-live "boot=live components console=ttyS0,115200n8" \
   --archive-areas "main restricted universe multiverse" \
   --mirror-bootstrap "${MIRROR}" \
